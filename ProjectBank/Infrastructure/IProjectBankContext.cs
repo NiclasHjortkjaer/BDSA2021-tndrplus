9893317bab5c2109b3ab69f039a128e7a9100398
Akongstad <kongstad@outlook.com>
2021-11-24 15:14:07 +0100

    Initialize infrastructure v0.1. - Add entities and attributes - init DbContext - Add IDbContext - Delete Program.cs(Hello world)

---
+++ ProjectBank/Infrastructure/IProjectBankContext.cs
@@ -0,0 +1,10 @@
+namespace Infrastructure;
+
+public interface IProjectBankContext : IDisposable
+{
+    public DbSet<Account> Accounts { get; }
+    public DbSet<Keyword> Keywords { get; }
+    public DbSet<Project> Projects { get; }
+    int SaveChanges();
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+}
