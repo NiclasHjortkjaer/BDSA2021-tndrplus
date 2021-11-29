7c28ea3664bcfb317d1ff9e29830a2a436fb86fe
Akongstad <90042173+Akongstad@users.noreply.github.com>
2021-11-29 11:52:18 +0100

    Account repo (#16)
    * - Setup repositories with unimplemented methods
    - Begin preparing Infrastructure.tests
    
    * - AccountRepository.cs + tests
    - Create (revisit)
    - Read
    - Readall
    
    * -Accountrepo 0.1 finished. CRUD implemented
    - AccountRepositoryTests.cs with tests implemented for CRUD methods
    - AccountRepositoryTests.cs with implements disposable pattern

--- ProjectBank/Infrastructure/AccountRepository.cs
+++ ProjectBank/Infrastructure/AccountRepository.cs
@@ -1,5 +1,4 @@
 namespace ProjectBank.Infrastructure;
-
 public class AccountRepository : IAccountRepository
 {
     private readonly IProjectBankContext _context;
@@ -9,29 +8,80 @@ public class AccountRepository : IAccountRepository
         _context = context;
     }
 
-    public Task<AccountDto> CreateAsync(AccountCreateDto account)
+    public async Task<AccountDetailsDto> CreateAsync(AccountCreateDto account)
     {
-        throw new NotImplementedException();
+        var newAccount = new Account(account.AzureAAdToken)
+        {
+            AccountType = account.AccountType,
+            SavedProjects = await GetSavedProjectsAsync(account.SavedProjects).ToListAsync()
+        };
+        _context.Accounts.Add(newAccount);
+        await _context.SaveChangesAsync();
+        return new AccountDetailsDto(
+            newAccount.Id,
+            newAccount.AzureAdToken,
+            newAccount.AccountType,
+            newAccount.SavedProjects.Select(a => a.Title).ToHashSet());
     }
 
-    public Task<AccountDto> ReadAsync(int accountId)
+    public async Task<AccountDetailsDto> ReadAsync(int accountId)
     {
-        throw new NotImplementedException();
+        var accounts = from a in _context.Accounts
+            where a.Id == accountId
+            select new AccountDetailsDto(
+                a.Id,
+                a.AzureAdToken,
+                a.AccountType,
+                a.SavedProjects.Select(p => p.Title).ToHashSet()
+            );
+
+        return await accounts.FirstOrDefaultAsync();
+    }
+    public async Task<IReadOnlyCollection<AccountDto>> ReadAllAsync() =>
+        (await _context.Accounts
+            .Select(a => new AccountDto(a.Id, a.AzureAdToken, a.AccountType))
+            .ToListAsync())
+            .AsReadOnly();
+
+    public async Task<Status> UpdateAsync(int id, AccountUpdateDto account)
+    {
+        var entitiy = await _context.Accounts.FirstOrDefaultAsync(a => a.Id == id);
+
+        if (entitiy == null)
+        {
+            return Status.NotFound;
+        }
+
+        entitiy.AccountType = account.AccountType;
+        entitiy.AzureAdToken = account.AzureAAdToken;
+        entitiy.SavedProjects = await GetSavedProjectsAsync(account.SavedProjects).ToListAsync();
+        
+        await _context.SaveChangesAsync();
+        return Status.Updated;
     }
 
-    public Task<IReadOnlyCollection<AccountDto>> ReadAllAsync()
+    public async Task<Status> DeleteAsync(int accountId)
     {
-        throw new NotImplementedException();
-    }
+        var account = await _context.Accounts.FindAsync(accountId);
+        if (account == null)
+        {
+            return Status.NotFound;
+        }
 
-    public Task<Status> UpdateAsync(int id, AccountUpdateDto account)
-    {
-        throw new NotImplementedException();
+        _context.Accounts.Remove(account);
+        await _context.SaveChangesAsync();
+        
+        return Status.Deleted;
     }
-
-    public Task<Status> DeleteAsync(int accountId)
+    
+    private async IAsyncEnumerable<Project> GetSavedProjectsAsync(IEnumerable<string> projects)
     {
-        throw new NotImplementedException();
+        var existing = await _context.Projects.Where(p => projects.Contains(p.Title)).ToDictionaryAsync(p => p.Title);
+
+        foreach (var project in projects)
+        {
+            yield return existing.TryGetValue(project, out var p) ? p : new Project(project);
+        }
     }
 }
 
--- ProjectBank/Infrastructure/GlobalUsing.cs
+++ ProjectBank/Infrastructure/GlobalUsing.cs
@@ -1,3 +1,6 @@
 global using Microsoft.EntityFrameworkCore;
 global using System.ComponentModel.DataAnnotations;
 global using ProjectBank.Core;
+global using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+global using System.ComponentModel.DataAnnotations;
+
