3ef6389ade43e1da96f3189d2949e593018df558
Christian Emil Stender <ches@itu.dk>
2021-12-01 15:02:17 +0100

    Server controller tests

--- ProjectBank/Server.test/KeywordControllerTests.cs
+++ ProjectBank/Server.test/KeywordControllerTests.cs
@@ -5,8 +5,138 @@ namespace ProjectBank.Server.test;
 public class KeywordControllerTests
 {
     [Fact]
-    public void Test1()
+    public async Task Create_creates_keyword()
     {
+        // Arrange
+        var logger = new Mock<ILogger<KeywordController>>();
+        var toCreate = new KeywordCreateDto();
+        var created = new KeywordDto(1, "Word");
+        var repository = new Mock<IKeywordRepository>();
+        repository.Setup(m => m.CreateAsync(toCreate)).ReturnsAsync(created);
+        var controller = new KeywordController(logger.Object, repository.Object);
 
+        // Act
+        var result = await controller.Post(toCreate) as CreatedAtRouteResult;
+
+        // Assert
+        Assert.Equal(created, result?.Value);
+        Assert.Equal("Get", result?.RouteName);
+        Assert.Equal(KeyValuePair.Create("Id", (object?)1), result?.RouteValues?.Single());
+    }  
+
+    [Fact]
+    public async Task Get_returns_keyword_from_repo()
+    {
+        // Arrange
+        var logger = new Mock<ILogger<KeywordController>>();
+        var expected = Array.Empty<KeywordDto>();
+        var repository = new Mock<IKeywordRepository>();
+        repository.Setup(m => m.ReadAllAsync()).ReturnsAsync(expected);
+        var controller = new KeywordController(logger.Object, repository.Object);
+
+        // Act
+        var actual = await controller.Get();
+
+        // Assert
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public async Task Get_given_non_existing_returns_NotFound()
+    {
+        // Arrange
+        var logger = new Mock<ILogger<KeywordController>>();
+        var repository = new Mock<IKeywordRepository>();
+        repository.Setup(m => m.ReadAsync(42)).ReturnsAsync(default(KeywordDto));
+        var controller = new KeywordController(logger.Object, repository.Object);
+
+        // Act
+        var response = await controller.Get(42);
+
+        // Assert
+        Assert.IsType<NotFoundResult>(response.Result);
+    }
+
+    [Fact]
+    public async Task Get_given_existing_returns_keyword()
+    {
+        // Arrange
+        var logger = new Mock<ILogger<KeywordController>>();
+        var repository = new Mock<IKeywordRepository>();
+        var keyword = new KeywordDto(1, "Word");
+        repository.Setup(m => m.ReadAsync(1)).ReturnsAsync(keyword);
+        var controller = new KeywordController(logger.Object, repository.Object);
+
+        // Act
+        var response = await controller.Get(1);
+
+        // Assert
+        Assert.Equal(keyword, response.Value);
+    }
+
+    [Fact]
+    public async Task Put_given_unknown_id_returns_NotFound()
+    {
+        // Arrange
+        var logger = new Mock<ILogger<KeywordController>>();
+        var keyword = new KeywordUpdateDto();
+        var repository = new Mock<IKeywordRepository>();
+        repository.Setup(m => m.UpdateAsync(1, keyword)).ReturnsAsync(NotFound);
+        var controller = new KeywordController(logger.Object, repository.Object);
+
+        // Act
+        var response = await controller.Put(1, keyword);
+
+        // Assert
+        Assert.IsType<NotFoundResult>(response);
+    }
+
+    [Fact]
+    public async Task Put_updates_keyword()
+    {
+        // Arrange
+        var logger = new Mock<ILogger<KeywordController>>();
+        var keyword = new KeywordUpdateDto();
+        var repository = new Mock<IKeywordRepository>();
+        repository.Setup(m => m.UpdateAsync(1, keyword)).ReturnsAsync(Updated);
+        var controller = new KeywordController(logger.Object, repository.Object);
+
+        // Act
+        var response = await controller.Put(1, keyword);
+
+        // Assert
+        Assert.IsType<NoContentResult>(response);
+    }
+
+    [Fact]
+    public async Task Delete_given_non_existing_returns_NotFound()
+    {
+        // Arrange
+        var logger = new Mock<ILogger<KeywordController>>();
+        var repository = new Mock<IKeywordRepository>();
+        repository.Setup(m => m.DeleteAsync(42)).ReturnsAsync(Status.NotFound);
+        var controller = new KeywordController(logger.Object, repository.Object);
+
+        // Act
+        var response = await controller.Delete(42);
+
+        // Assert
+        Assert.IsType<NotFoundResult>(response);
+    }
+
+    [Fact]
+    public async Task Delete_given_existing_returns_NoContent()
+    {
+        // Arrange
+        var logger = new Mock<ILogger<KeywordController>>();
+        var repository = new Mock<IKeywordRepository>();
+        repository.Setup(m => m.DeleteAsync(1)).ReturnsAsync(Status.Deleted);
+        var controller = new KeywordController(logger.Object, repository.Object);
+
+        // Act
+        var response = await controller.Delete(1);
+
+        // Assert
+        Assert.IsType<NoContentResult>(response);
     }
 }
--- ProjectBank/Server.test/ProjectControllerTests.cs
+++ ProjectBank/Server.test/ProjectControllerTests.cs
@@ -5,8 +5,138 @@ namespace ProjectBank.Server.test;
 public class ProjectControllerTests
 {
     [Fact]
-    public void Test1()
+    public async Task Create_creates_project()
     {
+        // Arrange
+        var logger = new Mock<ILogger<ProjectController>>();
+        var toCreate = new ProjectCreateDto();
+        var created = new ProjectDetailsDto(1, new AccountDto(1, "AzureAdToken", AccountType.Supervisor, new HashSet<ProjectDto>()), Degree.Bachelor, "Title", "Description", "ImageUrl", "Body", 15, new HashSet<KeywordDto>());
+        var repository = new Mock<IProjectRepository>();
+        repository.Setup(m => m.CreateAsync(toCreate)).ReturnsAsync(created);
+        var controller = new ProjectController(logger.Object, repository.Object);
 
+        // Act
+        var result = await controller.Post(toCreate) as CreatedAtRouteResult;
+
+        // Assert
+        Assert.Equal(created, result?.Value);
+        Assert.Equal("Get", result?.RouteName);
+        Assert.Equal(KeyValuePair.Create("Id", (object?)1), result?.RouteValues?.Single());
+    }  
+
+    [Fact]
+    public async Task Get_returns_projects_from_repo()
+    {
+        // Arrange
+        var logger = new Mock<ILogger<ProjectController>>();
+        var expected = Array.Empty<ProjectDto>();
+        var repository = new Mock<IProjectRepository>();
+        repository.Setup(m => m.ReadAllAsync()).ReturnsAsync(expected);
+        var controller = new ProjectController(logger.Object, repository.Object);
+
+        // Act
+        var actual = await controller.Get();
+
+        // Assert
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public async Task Get_given_non_existing_returns_NotFound()
+    {
+        // Arrange
+        var logger = new Mock<ILogger<ProjectController>>();
+        var repository = new Mock<IProjectRepository>();
+        repository.Setup(m => m.ReadAsync(42)).ReturnsAsync(default(ProjectDetailsDto));
+        var controller = new ProjectController(logger.Object, repository.Object);
+
+        // Act
+        var response = await controller.Get(42);
+
+        // Assert
+        Assert.IsType<NotFoundResult>(response.Result);
+    }
+
+    [Fact]
+    public async Task Get_given_existing_returns_project()
+    {
+        // Arrange
+        var logger = new Mock<ILogger<ProjectController>>();
+        var repository = new Mock<IProjectRepository>();
+        var project = new ProjectDetailsDto(1, new AccountDto(1, "AzureAdToken", AccountType.Supervisor, new HashSet<ProjectDto>()), Degree.Bachelor, "Title", "Description", "ImageUrl", "Body", 15, new HashSet<KeywordDto>());
+        repository.Setup(m => m.ReadAsync(1)).ReturnsAsync(project);
+        var controller = new ProjectController(logger.Object, repository.Object);
+
+        // Act
+        var response = await controller.Get(1);
+
+        // Assert
+        Assert.Equal(project, response.Value);
+    }
+
+    [Fact]
+    public async Task Put_given_unknown_id_returns_NotFound()
+    {
+        // Arrange
+        var logger = new Mock<ILogger<ProjectController>>();
+        var project = new ProjectUpdateDto();
+        var repository = new Mock<IProjectRepository>();
+        repository.Setup(m => m.UpdateAsync(1, project)).ReturnsAsync(NotFound);
+        var controller = new ProjectController(logger.Object, repository.Object);
+
+        // Act
+        var response = await controller.Put(1, project);
+
+        // Assert
+        Assert.IsType<NotFoundResult>(response);
+    }
+
+    [Fact]
+    public async Task Put_updates_project()
+    {
+        // Arrange
+        var logger = new Mock<ILogger<ProjectController>>();
+        var project = new ProjectUpdateDto();
+        var repository = new Mock<IProjectRepository>();
+        repository.Setup(m => m.UpdateAsync(1, project)).ReturnsAsync(Updated);
+        var controller = new ProjectController(logger.Object, repository.Object);
+
+        // Act
+        var response = await controller.Put(1, project);
+
+        // Assert
+        Assert.IsType<NoContentResult>(response);
+    }
+
+    [Fact]
+    public async Task Delete_given_non_existing_returns_NotFound()
+    {
+        // Arrange
+        var logger = new Mock<ILogger<ProjectController>>();
+        var repository = new Mock<IProjectRepository>();
+        repository.Setup(m => m.DeleteAsync(42)).ReturnsAsync(Status.NotFound);
+        var controller = new ProjectController(logger.Object, repository.Object);
+
+        // Act
+        var response = await controller.Delete(42);
+
+        // Assert
+        Assert.IsType<NotFoundResult>(response);
+    }
+
+    [Fact]
+    public async Task Delete_given_existing_returns_NoContent()
+    {
+        // Arrange
+        var logger = new Mock<ILogger<ProjectController>>();
+        var repository = new Mock<IProjectRepository>();
+        repository.Setup(m => m.DeleteAsync(1)).ReturnsAsync(Status.Deleted);
+        var controller = new ProjectController(logger.Object, repository.Object);
+
+        // Act
+        var response = await controller.Delete(1);
+
+        // Assert
+        Assert.IsType<NoContentResult>(response);
     }
 }
