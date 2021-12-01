9b2c074159a4a8e651dfcb4c8c61b7a6bc0451a8
Christian Emil Stender <ches@itu.dk>
2021-12-01 15:06:16 +0100

    Controllers almost implemented for API

--- ProjectBank/Server/Controllers/AccountController.cs
+++ ProjectBank/Server/Controllers/AccountController.cs
@@ -13,5 +13,42 @@ public class AccountController : ControllerBase
     {
         _logger = logger;
         _repository = repository;
+    }    
+
+    [AllowAnonymous]
+    [HttpGet]
+    public async Task<IReadOnlyCollection<AccountDto>> Get()
+        => await _repository.ReadAllAsync();
+
+    [AllowAnonymous]
+    [HttpGet("{id}")]
+    [ProducesResponseType(404)]
+    [ProducesResponseType(typeof(AccountDto), 200)]
+    public async Task<ActionResult<AccountDto>> Get(int accountId)
+       => throw new NotImplementedException();
+       // => (await _repository.ReadAsync(accountId)).ToActionResult();
+
+    [Authorize]
+    [HttpPost]
+    [ProducesResponseType(typeof(AccountDto), 201)]
+    public async Task<IActionResult> Post(AccountCreateDto account)
+    {
+        var created = await _repository.CreateAsync(account);
+
+        return CreatedAtRoute(nameof(Get), new { created.Id }, created);
     }
+
+    [Authorize]
+    [HttpPut("{id}")]
+    [ProducesResponseType(204)]
+    [ProducesResponseType(404)]
+    public async Task<IActionResult> Put(int id, [FromBody] AccountUpdateDto account)
+        => (await _repository.UpdateAsync(id, account)).ToActionResult();
+
+    [Authorize]
+    [HttpDelete("{id}")]
+    [ProducesResponseType(204)]
+    [ProducesResponseType(404)]
+    public async Task<IActionResult> Delete(int id)
+        => (await _repository.DeleteAsync(id)).ToActionResult();
 }
