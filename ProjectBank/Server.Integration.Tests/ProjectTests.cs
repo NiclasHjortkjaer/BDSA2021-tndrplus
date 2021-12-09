2533cba72d890a175c783efd2553382959e2c7ce
NiclasHjortkjaer <32992549+NiclasHjortkjaer@users.noreply.github.com>
2021-12-09 19:57:36 +0100

    Dev2 (#61)
    * implement ReadAuthorAsync in ProjectRepository
    
    * add readauthorasync
    
    * ProjectController GetProjectGivenController
    
    * searchUI can find projects given title
    projectrepository can also find projects given author
    
    * integration test on get by title
    
    * Get project by author in searchUI
    
    Co-authored-by: Niclas Søgaard Hjortkjær <nihj@itu.dk>

--- ProjectBank/Server.Integration.Tests/ProjectTests.cs
+++ ProjectBank/Server.Integration.Tests/ProjectTests.cs
@@ -66,10 +66,25 @@ public class ProjectTests : IClassFixture<CustomWebApplicationFactory>
     }
 
     [Fact]
-    public async Task Get_by_title_returns_projects_given_valid_title()
+    public async Task Get_by_input_returns_projects_given_valid_title()
     {
         var title = "Artificial Intelligence 101";
         var projects = await _client.GetFromJsonAsync<ProjectDetailsDto[]?>($"/api/Project/{title}");
+
+        Assert.NotNull(projects);
+        Assert.Equal(1, projects!.Length);
+
+        var project = projects.FirstOrDefault();
+        Assert.Equal("Artificial Intelligence 101", project!.Title);
+        Assert.Equal("Elon Musk", project.AuthorName);
+        Assert.NotEmpty(project.Keywords);
+    }
+
+    [Fact]
+    public async Task Get_by_input_returns_projects_given_valid_author()
+    {
+        var author = "Elon";
+        var projects = await _client.GetFromJsonAsync<ProjectDetailsDto[]?>($"/api/Project/{author}");
 
         Assert.NotNull(projects);
         Assert.Equal(1, projects!.Length);
