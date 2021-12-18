d755b0c5d3aba215a0e9250e2e020ad5d1ce7a2a
Niclas Søgaard Hjortkjær <nihj@itu.dk>
2021-12-18 18:18:12 +0100

    implement ikeywordfinder tests

--- ProjectBank/Client/IKeywordFinder.cs
+++ ProjectBank/Client/IKeywordFinder.cs
@@ -4,6 +4,7 @@ namespace ProjectBank.Client;
 
 public interface IKeywordFinder
 {
+    IDictionary<string, int> Ratios { get; }
     Task Setup(HttpClient Http);
     string FindWeightedRandomKeyword();
     Status UpdateRatioAsync(string keywordName, bool userLikedProject);
