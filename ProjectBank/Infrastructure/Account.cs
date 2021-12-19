43388e46b00f648db8e618ca2f9a22bea0cb8195
Akongstad <kongstad@outlook.com>
2021-12-19 16:06:04 +0100

    Restrains on string length

--- ProjectBank/Infrastructure/Account.cs
+++ ProjectBank/Infrastructure/Account.cs
@@ -10,12 +10,12 @@ public class Account
     public int Id { get; set;}
 
     [StringLength(50)]
-
-   
     public string Name { get; set; }
 
+    [StringLength(200)]
     public string AzureAdToken { get; set; }
     
+    [StringLength(250)]
     public string? PictureUrl { get; set; }
     public ICollection<Project> AuthoredProjects { get; set; } = null!;
 
