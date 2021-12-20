451e5fc54d6f4c7225b04f9d472f954b0f939461
Carl <carl99@live.dk>
2021-12-20 11:34:55 +0100

    - added comment on the ProjectStateContainer, and correct author comment.

--- ProjectBank/Client/ProjectStateChanged.cs
+++ ProjectBank/Client/ProjectStateChanged.cs
@@ -1,5 +1,13 @@
 ﻿namespace ProjectBank.Client;
-//this approach is highly inspired by Quanqo from StackOverflow, and Chris Sainty from his own website. links below:
+//
+//this approach is highly inspired by Quanqo from StackOverflow, and Chris Sainty from his own website.
+//Creates a state container to manage communication between components in an observer pattern.
+//NavMenu subscribes to the OnChange event of this class, it "observes" for changes.
+//When the swipe page is updated, the updated state is given to this class, which invokes the onChange event.
+//the NavMenu then checks the updated data of this class and re-renders accordingly, in a dynamic sense.
+//
+//
+//links below:
 //https://chrissainty.com/3-ways-to-communicate-between-components-in-blazor/
 //https://stackoverflow.com/questions/60912726/exchange-data-between-page-and-navmenu-in-blazor
 public class ProjectStateChanged
@@ -36,6 +44,6 @@ public class ProjectStateChanged
             }
         }
     }
-    //The NavMenu "listens" to the OnChange being invoked, then it will re-render the page based on the new data in this state container class.
+    
     private void NotifyStateChanged() => OnChange?.Invoke();
 }
