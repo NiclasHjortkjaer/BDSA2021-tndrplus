8be441fd84547378e568b2b2b6f400f5631cd239
Carl <carl99@live.dk>
2021-12-20 11:19:14 +0100

    - Remove some extra comments

--- ProjectBank/Client/ProjectStateChanged.cs
+++ ProjectBank/Client/ProjectStateChanged.cs
@@ -1,43 +1,31 @@
 ﻿namespace ProjectBank.Client;
-//this approach is highly inspired by Quanqo from StackOverflow, and Chris Sainty. links below:
+//this approach is highly inspired by Quanqo from StackOverflow, and Chris Sainty from his own website. links below:
 //https://chrissainty.com/3-ways-to-communicate-between-components-in-blazor/
 //https://stackoverflow.com/questions/60912726/exchange-data-between-page-and-navmenu-in-blazor
-public class ProjectStateChanged : IDisposable
+public class ProjectStateChanged
 {
     public ProjectStateChanged()
     {
         projects = new List<string>();
     }
-
     public List<string> projects { get; private set; }
-
     public event Action OnChange;
-    
     public void AddSavedProjects(string title)
     {
         if (!projects.Contains(title))
         {
-            
             projects.Add(title);
-            NotifyStateChanged(); 
-            
+            NotifyStateChanged();
         }
-        
     }
-    
-
     public void RemoveSavedProjects(string title)
     {
         if (projects.Contains(title))
         {
-            
-             projects.Remove(title);
-             NotifyStateChanged(); 
-                  
+            projects.Remove(title);
+             NotifyStateChanged();
         }
-        
     }
-    
     public void setList(ISet<string> titles)
     {
         foreach (string s in titles)
@@ -48,12 +36,6 @@ public class ProjectStateChanged : IDisposable
             }
         }
     }
-
-    //The NavMenu "listens" to the OnChange being invoked, the it will re-render the page based on the new data in this state container class.
+    //The NavMenu "listens" to the OnChange being invoked, then it will re-render the page based on the new data in this state container class.
     private void NotifyStateChanged() => OnChange?.Invoke();
-
-    public void Dispose()
-    {
-        this.Dispose();
-    }
 }
