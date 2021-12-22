3bd5c8864a860d7c526809568c0bcd8dc50a4f85
Stender98 <90451897+Stender98@users.noreply.github.com>
2021-12-22 01:21:58 +0100

    fixes (#165)
    Co-authored-by: Christian Emil Stender <ches@itu.dk>

--- ProjectBank/Server/Extensions/Extensions.cs
+++ ProjectBank/Server/Extensions/Extensions.cs
@@ -1,6 +1,9 @@
 namespace ProjectBank.Server.Extensions;
+
 public static class Extensions
 {
+    //Extension method for IActionResult return type used for multiple ActionResults
+    //Taken from Rasmus Lystrøm's BDSA2021 Repository on GitHub: https://github.com/ondfisk/BDSA2021/tree/main/MyApp.Server/Model
     public static IActionResult ToActionResult(this Status status) 
             => status switch
             {
